f26a71b83b0f5eb0b87642cb072186207ae32617
Encho Belezirev <encho.belezirev@sap.com>
2017-12-15 22:03:31 +0200

    Implement post method

--- src/main/java/com/fmi/edu/online/bus/tickets/web/resources/TicketsResource.java
+++ src/main/java/com/fmi/edu/online/bus/tickets/web/resources/TicketsResource.java
@@ -1,9 +1,12 @@
 package com.fmi.edu.online.bus.tickets.web.resources;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -20,11 +23,24 @@ public class TicketsResource {
 
 	private static List<Ticket> tickets = new ArrayList<>();
 	
+	@GET
+	@Path("/{id}")
 	public Response getTicket(@PathParam("id") String id){
 		Ticket result = tickets.stream().filter(ticket -> ticket.getId().equals(id)).findFirst().orElse(null);
 		if (result == null) {
 			return Response.status(Status.NOT_FOUND).build();
 		}
 		return Response.status(Status.OK).entity(result).build();
+	}
+	
+	@GET
+	public Response getTickets(){
+		return Response.ok().build();
+	}
+	
+	@POST
+	public Response createTicket(Ticket ticket){
+		tickets.add(ticket);
+		return Response.created(URI.create("tickets/"+ ticket.getId())).build();
 	}
 }
