f417826d591cf9fdb6570b219480e9ede66ef091
Encho Belezirev <encho.belezirev@sap.com>
2017-12-16 22:25:00 +0200

    Improve TicketsResource

--- src/main/java/com/fmi/edu/online/bus/tickets/web/resources/TicketsResource.java
+++ src/main/java/com/fmi/edu/online/bus/tickets/web/resources/TicketsResource.java
@@ -11,6 +11,7 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
@@ -54,7 +55,7 @@ public class TicketsResource {
 	}
 
 	@POST
-	public Response createTicket(TicketDto ticketDto, HttpServletRequest req) {
+	public Response createTicket(TicketDto ticketDto, @Context HttpServletRequest req) {
 		try {
 			String authorizationHeader = getAuthorizationHeader(req);
 			
