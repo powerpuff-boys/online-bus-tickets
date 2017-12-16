b7c60abbe6e87cf3fcf98d7e9fb4bdc89af216bc
Encho Belezirev <encho.belezirev@sap.com>
2017-12-16 17:09:28 +0200

    Improve rest

--- src/main/java/com/fmi/edu/online/bus/tickets/web/resources/TicketsResource.java
+++ src/main/java/com/fmi/edu/online/bus/tickets/web/resources/TicketsResource.java
@@ -1,12 +1,15 @@
 package com.fmi.edu.online.bus.tickets.web.resources;
 
 import java.net.URI;
+import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -26,21 +29,37 @@ public class TicketsResource {
 	@GET
 	@Path("/{id}")
 	public Response getTicket(@PathParam("id") String id){
-		TicketDto result = tickets.stream().filter(ticket -> ticket.getId().equals(id)).findFirst().orElse(null);
+		TicketDto result = findInternal(id);
 		if (result == null) {
 			return Response.status(Status.NOT_FOUND).build();
 		}
 		return Response.status(Status.OK).entity(result).build();
 	}
-	
-	@GET
-	public Response getTickets(){
-		return Response.ok().build();
+
+	private TicketDto findInternal(String id) {
+		return tickets.stream().filter(ticket -> ticket.getId().equals(id)).findFirst().orElse(null);
 	}
 	
 	@POST
 	public Response createTicket(TicketDto ticket){
+		ticket.setId(UUID.randomUUID().toString());
 		tickets.add(ticket);
 		return Response.created(URI.create("tickets/"+ ticket.getId())).entity(ticket).build();
+	}
+	
+	@PUT
+	@Path("/{id}")
+	public Response updateTicket(@PathParam("id") String id, TicketDto ticket){
+		TicketDto result = findInternal(id);
+		if (result == null) {
+			return Response.status(Status.NOT_FOUND).entity(MessageFormat.format("Ticket with id {0} does not exist", id)).build();
+		}
+		result.setBusId(ticket.getBusId());
+		result.setChecked(ticket.isChecked());
+		result.setCreatedOn(ticket.getCreatedOn());
+		result.setExpiresOn(ticket.getExpiresOn());
+		tickets.remove(result);
+		tickets.add(result);
+		return Response.created(URI.create("tickets/"+ ticket.getId())).entity(result).build();
 	}
 }
