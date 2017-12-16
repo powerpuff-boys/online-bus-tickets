6e6aaec6f1d9882f91ec2569075f366b5b8fde7c
Stunji <s.ivanov.todorov@abv.bg>
2017-12-17 00:58:01 +0200

    Authentication to Authorization

--- src/main/java/com/fmi/edu/online/bus/tickets/Constants.java
+++ src/main/java/com/fmi/edu/online/bus/tickets/Constants.java
@@ -6,7 +6,7 @@ public class Constants {
 	public static final String BASIC_AUTHENTICATION_KEYWORD = "Basic ";
 
 
-	public static final String AUTHENTICATION_HEADER = "Authentication";
+	public static final String AUTHENTICATION_HEADER = "Authorization";
 
 	public static final String DEFAULT_TICKETS_API = "tickets/";
 }
