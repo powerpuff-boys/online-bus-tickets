147e162bcc492930be154f7e3dc1866b2a53ca4d
Stunji <s.ivanov.todorov@abv.bg>
2017-12-17 13:53:43 +0200

    busDao changes

--- src/main/java/com/fmi/edu/online/bus/tickets/dao/BusDao.java
+++ src/main/java/com/fmi/edu/online/bus/tickets/dao/BusDao.java
@@ -2,17 +2,19 @@ package com.fmi.edu.online.bus.tickets.dao;
 
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
 
 import com.fmi.edu.online.bus.tickets.model.Bus;
 
 public class BusDao {
 	private EntityManagerFactory emf = Persistence.createEntityManagerFactory("bus-tickets");
-	
-	public Bus find(String id){
+
+	public Bus find(String id) {
 		Bus result = null;
 		try (AutoclosableEntityManager autoclosableEntityManager = new AutoclosableEntityManager(
 				emf.createEntityManager())) {
-			result = autoclosableEntityManager.find(Bus.class, id);
+			Query query = autoclosableEntityManager.createNamedQuery("Select * from Bus where id = " + id);
+			result = (Bus) query.getSingleResult();
 		}
 		return result;
 	}
